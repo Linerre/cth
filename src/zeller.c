1f5f89217e0777fbead434a11913b1b9e2a73ad9
Linerre <leon.errelin@outlook.com>
2025-05-10 13:07:28 +1000

    Fix zeller

--- src/zeller.c
+++ src/zeller.c
@@ -10,7 +10,7 @@ char* WEEKDAY[7] = {
   "MON",
   "TUE",
   "WED",
-  "TUR",
+  "THU",
   "FRI",
 };
 
@@ -34,19 +34,39 @@ unsigned int MONTH[12] = {
  * h = (q + floor(13(m+1)/5) + K + floor(K/4) + floor(J/4) - 2J) mod 7
  * Where:
 
- * h is the day of the week (0 = Saturday, 1 = Sunday, ..., 6 = Friday)
- * q is the day of the month
- * m is the month (3 = March, 4 = April, ..., 14 = February) (Note: January and February are counted as months 13 and 14 of the previous year)
- * K is the year of the century (year % 100)
- * J is the zero-based century (year/100)
+ * - h is the day of the week (0 = Saturday, 1 = Sunday, ..., 6 = Friday)
+ * - q is the day of the month
+ * - m is the month (3 = March, 4 = April, ..., 14 = February)
+ * Note: January and February are counted as months 13 and 14 of the previous year
+ * - K is the year of the century (year % 100)
+ * - J is the zero-based century (year/100)
  */
-int main(int argc, char* argv[]) {
-  unsigned int h, q, m, K, J;
-  q = 10;
-  m = MONTH[5-3];
-  K = 2025 % 100;
-  J = 2025 / 100;
-  h = (unsigned int)(q + floor(13*(m+1)/5) + K + floor(K/4) + floor(J/4) - 2*J) % 7;
+unsigned int zeller(unsigned int q, unsigned int month, unsigned int year)
+{
+  unsigned int m, K, J;
+  m = MONTH[month-3];
+  K = year % 100;
+  J = year / 100;
+  return (unsigned int)(q+floor(13*(m+1)/5)+K+floor(K/4)+floor(J/4)-2*J) % 7;
+}
 
-  printf("The day of the week for 2025-05-10 is %s\n", WEEKDAY[h]);
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("Usage: ./zeller.out YYYY-MM-DD\n");
+    return 1;
+  }
+  unsigned int year, month, day, zm, h;
+  if (sscanf(argv[1], "%u-%u-%u", &year, &month, &day) != 3) {
+    printf("Invalid date format. Use YYYY-MM-DD\n");
+    return 2;
+  }
+  // adjust month
+  if (month == 1) zm = 13;
+  else if (month == 2) zm = 14;
+  else zm = month;
+
+  h = zeller(day, zm, year - (month < 3));
+
+  printf("The day of the week for %s is: %s\n", argv[1], WEEKDAY[h]);
+  return 0;
 }
